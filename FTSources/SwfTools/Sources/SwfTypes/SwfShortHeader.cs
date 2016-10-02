bab8c63bb50f378670648993eaf79eeaeb894356
BlackMATov <blackmatov@gmail.com>
2016-10-03 03:31:57 +0700

    remove swf version check. add projects to common solution

--- FTSources/SwfTools/Sources/SwfTypes/SwfShortHeader.cs
+++ FTSources/SwfTools/Sources/SwfTypes/SwfShortHeader.cs
@@ -7,14 +7,13 @@ namespace SwfTools.SwfTypes {
 		public uint   FileLength;
 
 		public static SwfShortHeader Read(SwfStreamReader reader) {
-			return CheckHeader(new SwfShortHeader{
+			return new SwfShortHeader{
 				Format     = new string(reader.ReadChars(3)),
 				Version    = reader.ReadByte(),
-				FileLength = reader.ReadUInt32()});
+				FileLength = reader.ReadUInt32()};
 		}
 
 		public static void Write(SwfShortHeader header, Stream stream) {
-			CheckHeader(header);
 			stream.WriteByte((byte)header.Format[0]);
 			stream.WriteByte((byte)header.Format[1]);
 			stream.WriteByte((byte)header.Format[2]);
@@ -30,19 +29,6 @@ namespace SwfTools.SwfTypes {
 				"SwfShortHeader. " +
 				"Format: {0}, Version: {1}, FileLength: {2}",
 				Format, Version, FileLength);
-		}
-
-		static SwfShortHeader CheckHeader(SwfShortHeader header) {
-			if ( string.IsNullOrEmpty(header.Format) || (header.Format != "FWS" && header.Format != "CWS") ) {
-				throw new System.Exception(string.Format(
-					"Incorrect swf header format: {0}",
-					header.Format));
-			} else if ( header.Version != 28 ) {
-				throw new System.Exception(string.Format(
-					"Incorrect swf header version: {0}",
-					header.Version));
-			}
-			return header;
 		}
 	}
 }
